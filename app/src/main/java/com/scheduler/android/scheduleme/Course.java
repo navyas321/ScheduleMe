00a3e844789ccbe938c88d44ed3e20933a68aee2
navyas321 <navyas321@gmail.com>
2016-09-20 15:09:03 -0500

    First commit

--- app/src/main/java/com/scheduler/android/scheduleme/Course.java
+++ app/src/main/java/com/scheduler/android/scheduleme/Course.java
@@ -1,5 +1,13 @@
 package com.scheduler.android.scheduleme;
 
 public class Course {
-    private st
+    private String mCourseName;
+    private int mCreditHours;
+    private String mLocation;
+
+    private class Schedule{
+        private enum String {
+            "Monday", "Tuesday", "Wednesday", "Thursday". "Friday", "Saturday", "Sunday"}
+
+    }
 }
