7059272ef8aed5c65ffa3c162cea4a5f655e5702
Sahil Gupta <sahil1105@hotmail.com>
2016-09-21 20:43:39 -0500

    Added a few more fields to the schedule class.

--- app/src/main/java/com/scheduler/android/scheduleme/Course.java
+++ app/src/main/java/com/scheduler/android/scheduleme/Course.java
@@ -1,16 +1,42 @@
 package com.scheduler.android.scheduleme;
 
+
+import java.sql.Time;
+
 public class Course {
-    private String mCourseName;
-    private int mCreditHours;
-    private String mLocation;
+
     enum Day {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY}
+
+    private String courseName;
+    private int creditHours;
+    private String lectureLocation;
+    private String discussionLocation;
+    private String labLocation;
+    private Schedule courseSchedule;
+
+    private boolean lectureComponent; //To validate if a lecture component exists
+    private boolean discussionComponent; //To validate if a discussion component exists
+    private boolean labComponent; //To validate if a lab component exists to the course
+
 
 
     private class Schedule{
 
 
-        private Day[] daysOfTheWeek;
+        Day[] daysOfTheWeekLecture;
+        Day[] daysOfTheWeekDiscussion;
+        Day[] daysOfTheWeekLab;
+
+        String lectureStartTime; //FORMAT: "HH:MM"
+        String lectureEndTime;
+
+        String discussionStartTime;
+        String discussionEndTime;
+
+        String labStartTime;
+        String labEndTIme;
+
+
 
 
 
