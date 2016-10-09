cae1c1b3066c865c5b7706f4d2b342d47132d7fc
navyas321 <navyas321@gmail.com>
2016-10-09 02:24:55 -0500

    Updated Activities

---
+++ app/src/main/java/com/scheduler/android/scheduleme/ImportantDatesActivity.java
@@ -0,0 +1,13 @@
+package com.scheduler.android.scheduleme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImportantDatesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_important_dates);
+    }
+}
